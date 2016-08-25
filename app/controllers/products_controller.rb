75f78b110d3f396964a5ee7a5fb84e3d13952de5
Eugeniu Ungureanu <e.ungureanu92@gmail.com>
2016-08-25 22:31:09 +0300

    Edit, update and destroy products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,44 @@ class ProductsController < ApplicationController
     @feed = Feed.find(params[:feed_id])
     @product = @feed.products.new
   end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def create
+    @feed = Feed.find(params[:feed_id])  
+    @product = @feed.products.new(product_params)
+
+    if @product.save
+      redirect_to @product
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to @product
+    else 
+      render "edit"
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    if @product.destroy
+      redirect_to feed_path(@product.feed_id)
+    else
+      redirect_to @product
+    end
+  end
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :aff_code, :price, :campaign_name, :image_urls, :description)
+  end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -30,4 +30,52 @@ class ProductsControllerTest < ActionController::TestCase
     assert assigns(:product).new_record?
     assert_response :success
   end
+
+  test "should create new product" do
+    user = User.create(email: "user@fm.md", password: "password")
+    feed = Feed.create
+    login(user)
+
+    assert_difference("Product.count", 1) do
+      post :create, feed_id: feed.id, product: {title: "test_string", aff_code: "test_string", price: "test_string", campaign_name: "test_string", image_urls: "test_string", description: "test_string"}
+    end
+
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  test "should get edit page" do
+    user = User.create(email: "user@fm.md", password: "password")
+    feed = Feed.create
+    product = feed.products.create
+    login(user)
+
+    get :edit, id: product.id
+
+    assert_response :success
+  end
+
+  test "should update a product" do
+    user = User.create(email: "user@fm.md", password: "password")
+    feed = Feed.create
+    product = feed.products.create
+    login(user)
+
+    patch :update, id: product.id, product: {title: "updated", aff_code: "updated", price: "updated", campaign_name: "updated", image_urls: "updated", description: "updated"}
+
+    assert_equal "updated", assigns(:product).title
+    assert_redirected_to product_path(assigns(:product))
+  end
+
+  test "should destroy a product" do
+    user = User.create(email: "user@fm.md", password: "password")
+    feed = Feed.create
+    product = feed.products.create
+    login(user)
+
+    assert_difference("Product.count", -1) do
+      delete :destroy, id: product.id 
+    end
+
+    assert_redirected_to feed_path(feed)
+  end
 end
