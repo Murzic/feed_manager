9098af8672c8fdca3e385a8b55e7b935e2621119
Eugeniu Ungureanu <e.ungureanu92@gmail.com>
2016-08-27 16:44:04 +0300

    Implement the csv feed importer

--- app/services/feed_import/csv.rb
+++ app/services/feed_import/csv.rb
@@ -1,10 +1,26 @@
+require "csv"
+
 class FeedImport::CSV
   def initialize(csv_file)
     @csv_file = csv_file
   end
 
   def call
-    
+    feed = Feed.create
+    products = []
+
+    CSV.foreach(csv_file, headers: true) do |el| 
+      products.push feed.products.new(
+        title: el["title"],
+        aff_code: el["aff_code"],
+        price: el["price"],
+        campaign_name: el["campaign_name"],
+        image_urls: el["image_urls"],
+        category: el["category"].try(:text),
+        description: el["description"]
+        )
+    end
+    Product.import products, validate: false
   end
 
   private
